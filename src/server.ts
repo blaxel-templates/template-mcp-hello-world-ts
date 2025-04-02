c9322f8989ccd0737c7819a39f2c055bd56ff66b
Thomas Crochet <thomas@gmail.com>
2025-04-02 15:33:15 +0200

    First commit

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import { BlaxelMcpServerTransport, logger } from "@blaxel/sdk";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: "{{.ProjectName}}",
+  version: "1.0.0",
+  description: ""
+});
+
+server.tool(
+  "hello_world",
+  "Say hello to a person",
+  {
+    firstname: z.string()
+  },
+  async ({firstname}) => {
+    logger.info(`Hello world called`);
+    return {
+      content: [{ type: "text", text: `Hello ${firstname}` }]
+    }
+  }
+);
+
+function main() {
+  let transport;
+  if (process.env.BL_SERVER_PORT) {
+    transport = new BlaxelMcpServerTransport();
+  } else {
+    transport = new StdioServerTransport();
+  }
+  server.connect(transport);
+  logger.info("Server started");
+}
+
+main();
